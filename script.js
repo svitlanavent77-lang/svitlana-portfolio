aa120a8649f991047acfcba4ec462ed4bc95e40c
Svitlana Vent <svitlana.vent@edu.turkuamk.fi>
2026-05-15 18:27:25 +0300

    fix hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-alert("JS connected");
 const menuToggle = document.querySelector(".menu-toggle");
 const navigation = document.querySelector(".navigation");
 
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", function () {
   navigation.classList.toggle("active");
 });
