bbf781c8e22d41499642e5a8581f463a43280b07
Svitlana Vent <svitlana.vent@edu.turkuamk.fi>
2026-05-15 18:32:09 +0300

    test

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navigation = document.querySelector(".navigation");
 
+console.log("Button:", menuToggle);
+console.log("Navigation:", navigation);
+
 menuToggle.addEventListener("click", function () {
+  alert("Button clicked");
+
   navigation.classList.toggle("active");
 });
